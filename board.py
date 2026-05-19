8fb4fdfc6c6674cb77e455b5d95d9b3fee21fcdf
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-19 10:20:33 +0300

    Fix create_widgets()
    +            col = 0
    +            stopwatch = Stopwatch(self.frame)
    +            stopwatch.grid(column=col, row=row, sticky=tk.NW)
    +            col += 1

--- board.py
+++ board.py
@@ -8,18 +8,23 @@ from tkinter_stopwatch import Stopwatch
 class Board(tk.Tk):
     def __init__(self, stopwatches):
         super().__init__()
-        self.title = "Stopwatches"
+        self.geometry("300x200")
+        self.title(f"Stopwatches({stopwatches})")
         self.stopwatches = stopwatches
         self.frame = tk.Frame(self)
         self.frame.grid(column=0, row=0, sticky=tk.NSEW)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
+        self.create_widgets()
 
     def create_widgets(self):
         cols = 2
         rows = self.stopwatches // cols
-        col = 0
         for row in range(rows):
+            col = 0
+            stopwatch = Stopwatch(self.frame)
+            stopwatch.grid(column=col, row=row, sticky=tk.NW)
+            col += 1
             stopwatch = Stopwatch(self.frame)
             stopwatch.grid(column=col, row=row, sticky=tk.NW)
             col += 1
