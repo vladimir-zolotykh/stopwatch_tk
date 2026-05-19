815d38bbdc1d00e10293241fc35be81b296062ed
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-19 10:12:49 +0300

    Initial import
    Add Board(tk.Tk) class

---
+++ board.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import tkinter as tk
+from tkinter_stopwatch import Stopwatch
+
+
+class Board(tk.Tk):
+    def __init__(self, stopwatches):
+        super().__init__()
+        self.title = "Stopwatches"
+        self.stopwatches = stopwatches
+        self.frame = tk.Frame(self)
+        self.frame.grid(column=0, row=0, sticky=tk.NSEW)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+    def create_widgets(self):
+        cols = 2
+        rows = self.stopwatches // cols
+        col = 0
+        for row in range(rows):
+            stopwatch = Stopwatch(self.frame)
+            stopwatch.grid(column=col, row=row, sticky=tk.NW)
+            col += 1
+
+
+if __name__ == "__main__":
+    board = Board(10)
+    board.mainloop()
